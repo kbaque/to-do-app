1c1bff0b3acb76f3dc3255ad53fa4d99061a03e7
Karen Baque <k.baque@aol.com>
2017-07-31 00:08:53 -0400

    installs

---
+++ db/seeds/todos.sql
@@ -0,0 +1,19 @@
+INSERT INTO todos (title, category, info, status) VALUES
+(
+ 'Go for a walk',
+ 'Health',
+ 'walk 1 mile',
+ 'Done'
+),
+(
+  'Buy milk',
+  'Shopping',
+  'buy one Gallon of whole milk',
+  'Done'
+),
+(
+  'Start project'
+  'Homework'
+  'must be done before next monday',
+  'Pending'
+);
