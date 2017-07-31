8942728e378acaec52346de0424845010e016b1c
Karen Baque <k.baque@aol.com>
2017-07-31 10:40:35 -0400

    olt

--- db/config.js
+++ db/config.js
@@ -10,8 +10,8 @@ let db;
 
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
   db = pgp({
-    database: 'todos_dev',
-    //database: 'adaquote_development', //heroku
+    // database: 'todos_dev',
+    database: 'adaquote_development', //heroku
     port: 5432,
     host: 'localhost',
   });
